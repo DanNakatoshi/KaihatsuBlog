1306eafe5ac30e2eac72c1ef21c7e6f67dd30cae
Dan Nakatoshi <traitotaku@gmail.com>
2024-12-12 00:51:09 -0500

    remove the BR from prism

--- src/routes/articles/[slug]/+page.svelte
+++ src/routes/articles/[slug]/+page.svelte
@@ -1,15 +1,27 @@
 <script>
 	import * as Card from '$lib/components/ui/card';
+	import 'prismjs/themes/prism-twilight.min.css';
+	import { onMount } from 'svelte';
 
 	let { data } = $props();
-	console.log(data.post);
+	let isClient = $state(false);
 
 	let post = data.post[0];
+
+	onMount(() => {
+		isClient = true;
+		if (isClient) {
+			import('prismjs').then((Prism) => {
+				import('prismjs/components/prism-javascript'); // For JavaScript syntax highlighting, for example
+				Prism.highlightAll();
+			});
+		}
+	});
 </script>
 
-<div class="container">
-	<div class="grid grid-cols-12 gap-2">
-		<Card.Root class="col-span-9">
+<div class="container max-w-full w-full mx-auto px-0 ">
+	<div class="grid grid-cols-12 gap-2 w-full">
+		<Card.Root class="col-span-12 md:col-span-9 ">
 			<Card.Header>
 				<Card.Title>{post.title.rendered}</Card.Title>
 				<Card.Description>Card Description</Card.Description>
@@ -21,19 +33,23 @@
 				<!--  -->
 			</Card.Footer>
 		</Card.Root>
-		<Card.Root  class="col-span-3">
+		<Card.Root class="col-span-12 md:col-span-3">
 			<Card.Header>
-			  <Card.Title>Card Title</Card.Title>
-			  <Card.Description>Card Description</Card.Description>
+				<Card.Title>Card Title</Card.Title>
+				<Card.Description>Card Description</Card.Description>
 			</Card.Header>
 			<Card.Content>
-			  <p>Card Content</p>
+				<p>Card Content</p>
 			</Card.Content>
 			<Card.Footer>
-			  <p>Card Footer</p>
+				<p>Card Footer</p>
 			</Card.Footer>
-		  </Card.Root>
+		</Card.Root>
 	</div>
-	{JSON.stringify(post)}
-	
+	<!-- {JSON.stringify(post)} -->
 </div>
+
+<style>
+
+
+</style>
