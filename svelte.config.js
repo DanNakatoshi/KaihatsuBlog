99bedc8be306b2a749532e0f872b6d85c090d9ad
Dan Nakatoshi <traitotaku@gmail.com>
2025-01-07 21:11:01 -0500

    update adoptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,29 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            // default options for the Node adapter
+            out: 'build', // output directory
+            precompress: false, // enable gzip precompression
+            envPrefix: 'SVELTEKIT_' // environment variable prefix
+        })
+    }
 };
 
 export default config;
