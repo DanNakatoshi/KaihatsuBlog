3a11c37c2b48820e0082cd188a185f5de64282fb
Dan Nakatoshi <traitotaku@gmail.com>
2025-08-10 11:13:47 -0400

    updated for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,32 @@
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter({
+// 			// default options for the Node adapter
+// 			out: 'build', // output directory
+// 			precompress: false, // enable gzip precompression
+// 			envPrefix: 'SVELTEKIT_' // environment variable prefix
+// 		})
+// 	}
+// };
+
+// export default config;
+
+
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			// default options for the Node adapter
-			out: 'build', // output directory
-			precompress: false, // enable gzip precompression
-			envPrefix: 'SVELTEKIT_' // environment variable prefix
-		})
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    // For standard Serverless Functions on Vercel:
+    adapter: adapter({ runtime: 'nodejs20.x' })
+    // If you want Edge Functions instead, use:
+    // adapter: adapter({ runtime: 'edge' })
+  }
 };
 
 export default config;
